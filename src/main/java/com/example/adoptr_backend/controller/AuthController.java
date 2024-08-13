58b9297153dde674f204f6d6e8f559cfbfb57e7b
Nicolas Morales <0nicolasmorales@gmail.com>
2024-08-13 19:13:01 -0300

    refact

--- src/main/java/com/example/adoptr_backend/controller/AuthController.java
+++ src/main/java/com/example/adoptr_backend/controller/AuthController.java
@@ -1,10 +1,11 @@
 package com.example.adoptr_backend.controller;
 
 import com.example.adoptr_backend.service.AuthService;
-import com.example.adoptr_backend.service.JwtService;
 import com.example.adoptr_backend.service.dto.request.UserDTOin;
 import com.example.adoptr_backend.service.dto.response.TokenResponse;
 import com.example.adoptr_backend.service.dto.response.UserDTO;
+import com.example.adoptr_backend.util.JwtUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,14 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
+@Tag(name = "Auth", description = "Auth Endpoints")
 public class AuthController {
 
     private final AuthService authService;
-    private final JwtService jwtService;
-
-    public AuthController(AuthService authService, JwtService jwtService) {
+    public AuthController(AuthService authService) {
         this.authService = authService;
-        this.jwtService = jwtService;
     }
 
     @PostMapping("/register")
@@ -33,7 +32,7 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<TokenResponse> login(@RequestBody UserDTOin userRequestDto) {
         UserDTO authenticatedUser = authService.authenticate(userRequestDto);
-        final String accessToken = jwtService.buildToken(authenticatedUser.getEmail());
+        final String accessToken = JwtUtil.buildToken(authenticatedUser.getEmail());
         TokenResponse tokenResponse = new TokenResponse(accessToken, authenticatedUser);
         return ResponseEntity.ok(tokenResponse);
     }
--- src/main/java/com/example/adoptr_backend/service/JwtService.java
+++
@@ -1,9 +0,0 @@
-package com.example.adoptr_backend.service;
-
-public interface JwtService {
-    String extractEmail(String token);
-
-    boolean isTokenValid(String token, String email);
-
-    String buildToken(String email);
-}
