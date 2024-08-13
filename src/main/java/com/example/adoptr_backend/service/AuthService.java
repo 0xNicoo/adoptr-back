a08a8635c314e5b41cbdd2af468d790d2a59af79
Nicolas Morales <0nicolasmorales@gmail.com>
2024-08-13 19:22:55 -0300

    a

--- src/main/java/com/example/adoptr_backend/service/AuthService.java
+++ src/main/java/com/example/adoptr_backend/service/AuthService.java
@@ -4,7 +4,17 @@ import com.example.adoptr_backend.service.dto.request.UserDTOin;
 import com.example.adoptr_backend.service.dto.response.UserDTO;
 
 public interface AuthService {
+
+    /**
+     * Registra un usuario
+     * @param dto
+     */
     void register(UserDTOin dto);
 
+    /**
+     * Autentica al usuario
+     * @param dto
+     * @return
+     */
     UserDTO authenticate(UserDTOin dto);
 }
