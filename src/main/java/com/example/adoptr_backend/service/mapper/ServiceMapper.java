f8c549dae6a9ca1f2542b46948ce4f7c2f2988c4
Nicolas Morales <0nicolasmorales@gmail.com>
2024-10-08 16:34:03 -0300

    fix

--- src/main/java/com/example/adoptr_backend/service/mapper/ServiceMapper.java
+++ src/main/java/com/example/adoptr_backend/service/mapper/ServiceMapper.java
@@ -3,13 +3,10 @@ package com.example.adoptr_backend.service.mapper;
 import com.example.adoptr_backend.model.Service;
 import com.example.adoptr_backend.service.dto.request.ServiceDTOin;
 import com.example.adoptr_backend.service.dto.response.ServiceDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface ServiceMapper extends EntityMapper<ServiceDTO, Service>{
     ServiceMapper MAPPER = Mappers.getMapper(ServiceMapper.class);
     Service toEntity(ServiceDTOin dto);
