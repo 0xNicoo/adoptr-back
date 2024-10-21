8cb0a0ec006304353b64d1eb7c3b1f67e3cecd7c
Nicolas Morales <0nicolasmorales@gmail.com>
2024-10-21 00:34:12 -0300

    fix: Report url

--- src/main/java/com/example/adoptr_backend/service/impl/report/PublicationReportStrategy.java
+++ src/main/java/com/example/adoptr_backend/service/impl/report/PublicationReportStrategy.java
@@ -15,6 +15,7 @@ import com.example.adoptr_backend.service.dto.response.*;
 import com.example.adoptr_backend.service.mapper.ReportMapper;
 import com.example.adoptr_backend.service.mapper.UserMapper;
 import com.example.adoptr_backend.util.AuthSupport;
+import com.example.adoptr_backend.util.ReportSupport;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -131,6 +132,7 @@ public class PublicationReportStrategy implements ReportStrategy{
 
         if(relatedReports != null){
             dto.setReportCount((long) relatedReports.size());
+            dto.setUrl(ReportSupport.buildPublicationURL(pub));
             List<ReportDTO> reportDTOs = relatedReports.stream().map(ReportMapper.MAPPER::toDto).toList();
             dto.setReports(reportDTOs);
             PublicationDTO publicationDTO = getPublicationDTO(pub);
