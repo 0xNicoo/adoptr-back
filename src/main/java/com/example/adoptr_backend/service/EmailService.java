21175af094e0c57dd5b9a2e35ca26c5b124b2882
Vicku Miranda <vikymiri@hotmail.com>
2024-11-07 02:00:54 -0300

    fix de nico

--- src/main/java/com/example/adoptr_backend/service/EmailService.java
+++ src/main/java/com/example/adoptr_backend/service/EmailService.java
@@ -1,7 +1,5 @@
 package com.example.adoptr_backend.service;
 
-import com.example.adoptr_backend.service.dto.request.EmailDTOin;
-
 public interface EmailService {
     void welcomeEmail(String to, String subject, String userName);
 }
